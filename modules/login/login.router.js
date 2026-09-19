7cda4c81ad73d674f9b2209940b122eaf19692f3
Arun <arun70840@gmail.com>
2026-09-19 15:12:48 +0530

    heartbeat added

--- modules/login/login.router.js
+++ modules/login/login.router.js
@@ -4,6 +4,11 @@ const  authMiddleware = require("../../middleware/auth.middleware");
 const loginController  = require("./login.controller"); 
 router.post("/", loginController.login);
 router.post("/browser-close", loginController.browserClose);
+router.post(
+    "/heartbeat",
+    authMiddleware.auth,
+    loginController.heartbeat
+);
 router.post("/:id", authMiddleware.auth, loginController.logout);
 
 
