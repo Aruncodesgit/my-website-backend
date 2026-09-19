175d93094493b2d003471afd43324be81e13045e
Arun <arun70840@gmail.com>
2026-09-19 15:18:32 +0530

    revereted

--- modules/login/login.router.js
+++ modules/login/login.router.js
@@ -4,11 +4,6 @@ const  authMiddleware = require("../../middleware/auth.middleware");
 const loginController  = require("./login.controller"); 
 router.post("/", loginController.login);
 router.post("/browser-close", loginController.browserClose);
-router.post(
-    "/heartbeat",
-    authMiddleware.auth,
-    loginController.heartbeat
-);
 router.post("/:id", authMiddleware.auth, loginController.logout);
 
 
